e0cd37603442a0793e8a1c26d32a4a5b5dc47ae6
GunPKate <gun@builk.com>
2022-11-14 21:33:13 +0700

    start react

---
+++ src/components/admin/AdminOreder.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AdminOrders() {
+  return <div>AdminOrders</div>;
+}
---
+++ src/components/users/MedicineDisplay.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MedicineDisplay() {
+  return <div>MedicineDisplay</div>;
+}
