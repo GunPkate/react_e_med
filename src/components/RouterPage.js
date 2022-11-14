3e5ea99010de72c7e9a3eb6370c8892c749bce85
GunPKate <gun@builk.com>
2022-11-14 21:43:55 +0700

    add path browser Router

--- src/components/RouterPage.js
+++ src/components/RouterPage.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Registration from "./Registration";
+import Cart from "./users/Cart";
+import Dashboard from "./users/Dashboard";
+import MedicineDisplay from "./users/MedicineDisplay";
+import Orders from "./users/Oreder";
+import Profile from "./users/Profile";
 
 export default function RouterPage() {
-  return <div>RouterPage</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Registration />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Orders />} />
+        <Route path="/" element={<Profile />} />
+        <Route path="/" element={<Cart />} />
+        {/* <Route path="/" element={< />} /> */}
+      </Routes>
+    </Router>
+  );
 }
